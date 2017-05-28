3571a4d68f1e75142e5005d44f8055e867103aa8
Rizwan Nawab <nawabrizwan7@gmail.com>
2017-05-28 14:13:24 +0530

    Add files via upload

---
+++ smart-mirror/data.js
@@ -0,0 +1,64 @@
+var app = angular.module("myApp", ['ngAnimate']); 
+
+app.controller("TimeCtrl", function($scope, $timeout) {				//Time component
+
+var TimeCtrl = function() {
+    $scope.clock = "loading clock..."; // initialise the time variable
+    $scope.tickInterval = 1000 //ms
+
+var tick = function( ){
+       $scope.clock = Date.now(); 
+       $timeout(tick, $scope.tickInterval);
+}
+   
+    $timeout(tick, $scope.tickInterval);
+}
+
+TimeCtrl();
+
+});
+
+app.controller('weatherCtrl',function($scope,$http){		//Weather Component
+	var vm = $scope;
+	var apiKey = "83a33bfe870abc07f0fa990d1f3b2407";	
+	var apiKey2 = "25bd7bc4f9fa9155e6198f13dded6d3c";
+	/*alternate api key = 25bd7bc4f9fa9155e6198f13dded6d3c*/
+	//var openWeatherUrl ="http://api.openweathermap.org/data/2.5/forecast/city?id=1278149&APPID="+ apiKey1;
+	var openWeatherUrl = "http://api.openweathermap.org/data/2.5/weather?id=1278149&APPID="+apiKey;
+
+
+	$http.get(openWeatherUrl).success(function(data){
+
+		vm.description =data.weather[0].description ;
+		vm.city = data.name + ", " + data.sys.country;
+		vm.mintemp = ((data.main.temp_min) - 273.15).toFixed(0);
+		vm.maxtemp = ((data.main.temp_max) - 273.15).toFixed(0);
+		vm.temp = ((data.main.temp) - 273.15).toFixed(0);
+		vm.pic = data.weather[0].icon;
+		vm.picsrc = "http://openweathermap.org/img/w/"+ vm.pic +".png"
+		});
+
+		
+});
+
+app.controller('PanelController', function(){		//Panel tabs controller
+			this.tab = 1;
+			this.selectTab = function(setTab){
+				this.tab = setTab;
+			};
+			this.isSelected = function(checkTab){
+				return this.tab === checkTab;
+			};
+			
+		});
+app.controller('mapCtrl',function(){
+	 $scope.initialize = function() {
+          var map = new google.maps.Map(document.getElementById('map'), {
+             center: {lat: -34.397, lng: 150.644},
+             zoom: 8
+          });
+       }    
+       
+       google.maps.event.addDomListener(window, 'load', $scope.initialize);   
+
+});
